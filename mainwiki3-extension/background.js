9488060a8e3f9a8f60d820cc1f01d4f145accee4
Tomás Antunes <tomasantunes@gmail.com>
2022-08-22 23:44:15 +0100

    Create extension

---
+++ mainwiki3-extension/background.js
@@ -0,0 +1,76 @@
+var urlApi = "https://mainwiki3.paken.xyz";
+var category_id = "";
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+        title: 'Save Selected Text to MainWiki3',
+        contexts: ["selection"],
+        id: "saveTextContextAction"
+    });
+
+    chrome.contextMenus.create({
+        title: "Save Image to MainWiki3", 
+        contexts: ["image", "link"], 
+        id: "saveImageContextAction"
+      });    
+});
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(sender.tab ?
+                  "from a content script:" + sender.tab.url :
+                  "from the extension");
+        if (request.category_id != "" && request.category_id != undefined) {
+            sendResponse({status: "OK"})
+        }
+        sendResponse({status: "NOK"})
+        console.log(request.category_id);
+        category_id = request.category_id;
+    }
+  );
+
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data)
+    });
+    return response.json();
+}
+    
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId == "saveTextContextAction") {
+        console.log(info.selectionText);
+        postData(urlApi + "/api/files/insert", {
+            title: "Note",
+            content: info.selectionText,
+            category: category_id,
+            tags: "",
+            extension: "txt"
+        })
+        .then((data) => {
+            console.log(data); // JSON data parsed by `data.json()` call
+        });
+    }
+
+    if (info.menuItemId == "saveImageContextAction") {
+        console.log(info.srcUrl);
+
+        postData(urlApi + "/api/upload-image-url", {
+            imageUrl: info.srcUrl,
+            category: category_id,
+            tags: ""
+        })
+        .then((data) => {
+            console.log(data); // JSON data parsed by `data.json()` call
+        });
+    }
+});
---
+++ mainwiki3-extension/script.js
@@ -0,0 +1,64 @@
+var url = "https://mainwiki3.paken.xyz";
+var categoriesList = [];
+
+console.log("x1");
+
+function createSelectOptions(categories) {
+    for (var i in categories) {
+        var c = categories[i];
+        console.log(c);
+        $("#category-selector").append(`<option value="${c.value}">${c.label}</option>`);
+    }   
+}
+
+
+$.get(url + "/api/categories/list")
+.then(function(data) {
+    console.log(data);
+    if (data.status == "OK") {
+        var categories = data.data;
+        var categories_to_add = [];
+        for (var i in categories) {
+          var menuItem = categories[i];
+          if (menuItem.parent_id == 1) {
+            var obj = {label: menuItem.name, value: menuItem.id};
+            categories_to_add.push(obj);
+            for (var j in categories) {
+              var menuItem2 = categories[j];
+              if (menuItem2.parent_id == obj.value) {
+                var obj2 = {label: ">>> " + menuItem2.name, value: menuItem2.id};
+                categories_to_add.push(obj2);
+                for (var k in categories) {
+                  var menuItem3 = categories[k];
+                  if (menuItem3.parent_id == obj2.value) {
+                    var obj3 = {label: ">>> >>> " + menuItem3.name, value: menuItem3.id};
+                    categories_to_add.push(obj3);
+                  }
+                }
+              }
+            }
+          }
+        }
+        categoriesList = categories_to_add;
+        createSelectOptions(categoriesList);
+        console.log(categoriesList);
+    }
+    else {
+        console.log(data.error);
+    }
+})
+.fail(function(err) {
+    console.log(err);
+});
+
+
+$("#category-selector").on("change", function(e) {
+    console.log("x1");
+    chrome.runtime.sendMessage({category_id: e.target.value}, function(response) {
+        console.log(response.status);
+    }); 
+});
+
+chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+    console.log(response.farewell);
+});
