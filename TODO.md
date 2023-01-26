513dc4ab2c26cfa7092f9cfdbbcc9b613edbfc39
Tomás Antunes <tomasantunes@gmail.com>
2023-01-26 14:34:05 +0000

    Create list of 10 largest files

--- TODO.md
+++ TODO.md
@@ -2,7 +2,6 @@
 
 - Change category listing to support infinite nesting levels
 - Apply tags to all files
-- Create list of 10 largest text files
 - Create wordcloud with 50 most common words excluding stopwords.
 - Create page with pinned files.
 - Add PDF files.
--- markdown-wiki2-frontend/src/components/Dashboard.js
+++ markdown-wiki2-frontend/src/components/Dashboard.js
@@ -8,6 +8,7 @@ export default function Dashboard() {
   const [listTop10Categories, setListTop10Categories] = useState([]);
   const [listTop10Tags, setListTop10Tags] = useState([]);
   const [list10MostRecent, setList10MostRecent] = useState([]);
+  const [list10Largest, setList10Largest] = useState([]);
 
   function load10RandomSentences() {
     axios
@@ -65,11 +66,29 @@ export default function Dashboard() {
       });
   }
 
+  function load10Largest() {
+    axios
+      .get(config.BACKEND_URL + "/api/get-10-largest")
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.status == "OK") {
+          setList10Largest(response.data.data);
+        }
+        else {
+          alert(response.data.error);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   useEffect(() => {
     load10RandomSentences();
     loadTop10Categories();
     loadTop10Tags();
     load10MostRecent();
+    load10Largest();
   }, []);
   return (
     <div className="col-md-10 full-min-height p-5">
@@ -98,6 +117,12 @@ export default function Dashboard() {
           <li key={index}><Link to={"/categories/" + item.category_id + "#" + item.id}>{item.title}</Link></li>
         ))}
       </ul>
+      <h3>Top 10 Largest Files</h3>
+      <ol className="list-top10-largest-files">
+        {list10Largest.map((item, index) => (
+          <li key={index}><Link to={"/categories/" + item.category_id + "#" + item.id}>{item.title}</Link></li>
+        ))}
+      </ol>
     </div>
   )
 }
