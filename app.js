693ca7e33eba3503cc7113786637644de78a4697
Tomás Antunes <tomasantunes@gmail.com>
2023-08-05 15:57:40 +0100

    Update

--- app.js
+++ app.js
@@ -1452,6 +1452,7 @@ app.get("/import-section", async (req, res) => {
   var exported_files_tags = fs.readFileSync("exported_files_tags.json");
   var exported_files_tags = JSON.parse(exported_files_tags);
 
+  /*
   var sql = "INSERT INTO categories (name, parent_id) VALUES (?, ?)";
   for (var i in exported_category) {
     var result = await con2.query(sql, [exported_category[i].name, 0]);
@@ -1463,9 +1464,10 @@ app.get("/import-section", async (req, res) => {
     var result = await con2.query(sql, [exported_tags[i].name]);
     console.log(result[0].insertId);
   }
+  */
 
   var sql = "INSERT INTO files (title, content, extension, category_id, path) VALUES (?, ?, ?, ?, ?)";
-  for (var i in exported_file) {
+  for (var i in exported_files) {
     var result = await con2.query(sql, [exported_files[i].title, exported_files[i].content, exported_files[i].extension, exported_files[i].category_id, exported_files[i].path]);
     console.log(result[0].insertId);
   }
