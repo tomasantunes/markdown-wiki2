b12547dafe880a4fbe1f7b7350f7419761235d5d
Tomás Antunes <tomasantunes@gmail.com>
2023-08-06 10:34:57 +0100

    Update

--- app.js
+++ app.js
@@ -1453,24 +1453,28 @@ app.get("/import-section", async (req, res) => {
   var exported_files_tags = JSON.parse(exported_files_tags);
 
   var old_to_new_category_ids = {};
+  var old_to_new_file_ids = {};
 
   var sql = "INSERT INTO categories (name, parent_id) VALUES (?, ?)";
   for (var i in exported_category) {
-    var result = await con2.query(sql, [exported_category[i].name, 0]);s
+    var result = await con2.query(sql, [exported_category[i].name, 1]);
+    console.log(result[0].insertId);
     old_to_new_category_ids[exported_category[i].id] = result[0].insertId;
   }
 
+  /*
   var sql = "INSERT INTO tags (name) VALUES (?)";
   for (var i in exported_tags) {
     var result = await con2.query(sql, [exported_tags[i].name]);
     console.log(result[0].insertId);
   }
-  
+  */
 
   var sql = "INSERT INTO files (title, content, extension, category_id, path) VALUES (?, ?, ?, ?, ?)";
   for (var i in exported_files) {
     var result = await con2.query(sql, [exported_files[i].title, exported_files[i].content, exported_files[i].extension, old_to_new_category_ids[exported_files[i].category_id], exported_files[i].path]);
     console.log(result[0].insertId);
+    old_to_new_file_ids[exported_files[i].id] = result[0].insertId;
   }
 
   var sql = "INSERT INTO files_tags (file_id, tag_id) VALUES (?, ?)";
