167df02b2e0751b96fbef899b012c242d0240374
Tomás Antunes <tomasantunes@gmail.com>
2025-09-20 13:29:43 +0100

    Add exported_media and update tag-bookmarks.js

--- scripts/tag-bookmarks/tag-bookmarks.js
+++ scripts/tag-bookmarks/tag-bookmarks.js
@@ -2,7 +2,7 @@ var mysql = require("mysql2");
 var fs = require("fs");
 var secretConfig = require("../../secret-config.json");
 
-var file = fs.readFileSync("bookmarks-out.json");
+var file = fs.readFileSync("bookmarks-out2.json");
 var bookmarks = JSON.parse(file);
 
 con = mysql.createPool({
@@ -14,12 +14,14 @@ con = mysql.createPool({
     database: secretConfig.DB_NAME,
     port: '/var/run/mysqld/mysqld.sock',
     multipleStatements: true,
+    charset: "utf8mb4"
   });
 
 console.log("Starting...");
 var queries = '';
 for (var i in bookmarks) {
     if (bookmarks[i].tags != '') {
+	bookmarks[i].tags.replace(/[\u0800-\uFFFF]/g, '');
         queries += mysql.format("UPDATE bookmarks SET tags = ? WHERE id = ?; ", [bookmarks[i].tags, bookmarks[i].id]);
     }
 }
